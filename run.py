ec68bf5286307baa41c75bed685e933339989b1e
Architect Rusty <rusty@r2d2.network>
2026-03-25 18:49:29 +0100

    chore(git): purge des logs locaux et medias de test

--- run.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-with open("test_output.log", "w") as f:
-    try:
-        output = subprocess.check_output(["cargo", "test", "--workspace"], stderr=subprocess.STDOUT)
-        f.write(output.decode())
-    except subprocess.CalledProcessError as e:
-        f.write(e.output.decode())
-        f.write("\nFAILED")
